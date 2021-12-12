77096e2d4b391c93c8afd8d36dfde3718877b534
dachengzi-software <48669822+dachengzi-software@users.noreply.github.com>
2021-12-12 23:29:28 +0800

    大转盘

--- app/src/main/java/cn/bigorange/wheel/ui/EditActivity.kt
+++ app/src/main/java/cn/bigorange/wheel/ui/EditActivity.kt
@@ -1,6 +1,8 @@
 package cn.bigorange.wheel.ui
 
 import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
 import cn.bigorange.common.BaseActivity
 import cn.bigorange.common.utils.ListUtils
 import cn.bigorange.wheel.database.DatabaseHelper
@@ -22,18 +24,32 @@ class EditActivity : BaseActivity<ActivityEditBinding>() {
         binding.header.setOnCommonTitleBackClickListener { finish() }
         binding.header.setOnCommonTitleIconSubClickListener {
             record?.let {
-                if (StringUtils.isEmpty(binding.etQuestion.text.toString())) {
+                val question = binding.etQuestion.text.toString();
+                if (StringUtils.isEmpty(question)) {
                     ToastUtils.showShort("请输入问题");
                     return@let
                 }
-                it.question = binding.etQuestion.text.toString()
-                it.optionList = ListUtils.strSplitToList(binding.etOptions.text.toString())
+                val optionList = ListUtils.strSplitToList(binding.etOptions.text.toString())
+                if (optionList == null || optionList.size < 2) {
+                    ToastUtils.showShort("请至少输入2个选项");
+                    return@let
+                }
+                it.question = question
+                it.optionList = optionList
                 DatabaseHelper.getInstance().updateRecordById(it)
                 val intent = Intent()
                 this@EditActivity.setResult(RESULT_OK, intent)
                 this@EditActivity.finish()
             }
         }
+        binding.etOptions.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s0: Editable) {
+                val tempOptionList = ListUtils.strSplitToList(s0.toString())
+                binding.tvAmount.text = tempOptionList.size.toString()
+            }
+        })
     }
 
     override fun initData() {
--- app/src/main/java/cn/bigorange/wheel/ui/InputActivity.kt
+++ app/src/main/java/cn/bigorange/wheel/ui/InputActivity.kt
@@ -1,6 +1,9 @@
 package cn.bigorange.wheel.ui
 
 import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
 import cn.bigorange.common.BaseActivity
 import cn.bigorange.common.utils.ListUtils
 import cn.bigorange.wheel.database.DatabaseHelper
@@ -22,19 +25,34 @@ class InputActivity : BaseActivity<ActivityInputBinding>() {
 
 
     override fun initListener() {
+        binding.header.setOnCommonTitleBackClickListener { finish() }
         binding.header.setOnCommonTitleIconSubClickListener {
-            if (StringUtils.isEmpty(binding.etQuestion.text.toString())) {
+            val question = binding.etQuestion.text.toString();
+            if (StringUtils.isEmpty(question)) {
                 ToastUtils.showShort("请输入问题");
                 return@setOnCommonTitleIconSubClickListener
             }
+            val optionList = ListUtils.strSplitToList(binding.etOptions.text.toString())
+            if (optionList == null || optionList.size < 2) {
+                ToastUtils.showShort("请至少输入2个选项");
+                return@setOnCommonTitleIconSubClickListener
+            }
             val record = Record()
-            record.question = binding.etQuestion.text.toString()
-            record.optionList = ListUtils.strSplitToList(binding.etOptions.text.toString())
+            record.question = question
+            record.optionList = optionList
             DatabaseHelper.getInstance().insertRecord(record);
             val intent = Intent()
             this@InputActivity.setResult(RESULT_OK, intent)
             this@InputActivity.finish()
         }
+        binding.etOptions.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s0: Editable) {
+                val tempOptionList = ListUtils.strSplitToList(s0.toString())
+                binding.tvAmount.text = tempOptionList.size.toString()
+            }
+        })
     }
 
 }
